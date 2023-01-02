7f39e14a6a62e3df19aece83cbad4b72ee792dc2
Kenneth Lee <kenneth.lee.developer@gmail.com>
2023-01-02 13:47:07 -0800

    Finished no_valid_url?

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -3,3 +3,9 @@
 def all_words_capitalized?(words)
   words.all? { |word| word == "#{word[0].upcase}#{word[1...word.length].downcase}" }
 end
+
+def no_valid_url?(urls)
+  valid_urls = ['com', 'net', 'io', 'org']
+
+  urls.none? { |url| valid_urls.include? url.split('.')[1] }
+end
