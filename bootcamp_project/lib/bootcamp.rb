e9ad60e35c14066910d51b6c5aa990305dec4ae0
Kenneth Lee <kenneth.lee.developer@gmail.com>
2023-01-02 16:20:43 -0800

    Almost done with bootcamp class, ruby uses a shared default value for hashes, unless we pass in a block to specify how to create a new default value for each key

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,4 +1,54 @@
 # frozen_string_literal: true
 
+# Bootcamp class, according to specs from a/A
 class Bootcamp
+  attr_reader :name, :slogan, :teachers, :students
+  attr_accessor :grades
+
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |hash, key| hash[key] = []}
+  end
+
+  def hire(new_teacher)
+    @teachers << new_teacher
+  end
+
+  def enroll(new_student)
+    if @students.length < @student_capacity
+      @students << new_student
+      true
+    else
+      false
+    end
+  end
+
+  def enrolled?(student)
+    @students.include? student
+  end
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if enrolled? student
+      @grades[student] << grade
+      true
+    else
+      false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    @grade[student].sum / @grades[student].length
+  end
 end
